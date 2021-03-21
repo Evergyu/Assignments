260c160f9a98f5f69d25c54f8c548754fa544717
Evergyu <proggyu@gmail.com>
2021-03-21 20:40:49 +0900

    20210321

---
+++ size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int **x;
+
+    printf("----2018038083----¿Ã¿Œ±‘----\n");
+
+    printf("sizeof(x) = %lu\n", sizeof(x)); 
+    printf("sizeof(*x) = %lu\n", sizeof(*x)); 
+    printf("sizeof(**x) = %lu\n", sizeof(**x));
+
+}  
