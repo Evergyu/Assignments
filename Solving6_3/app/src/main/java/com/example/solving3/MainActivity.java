acf97953bf1614d18d161cfcf0e6f647ad453ccb
iingy <Evergyu@github.com>
2022-04-14 00:04:14 +0900

    Completed Practices

--- Solving6_3/app/src/main/java/com/example/solving3/MainActivity.java
+++ Solving6_3/app/src/main/java/com/example/solving3/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.solving3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.*;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setTitle("탭호스트 예제");
+
+        TabHost tabHost = findViewById(R.id.TabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpecCat = tabHost.newTabSpec("CAT").setIndicator("고양이");
+        tabSpecCat.setContent(R.id.cat);
+        tabHost.addTab(tabSpecCat);
+
+        TabHost.TabSpec tabSpecDog = tabHost.newTabSpec("DOG").setIndicator("강아지");
+        tabSpecDog.setContent(R.id.dog);
+        tabHost.addTab(tabSpecDog);
+
+        TabHost.TabSpec tabSpecRabbit = tabHost.newTabSpec("RABBIT").setIndicator("토끼");
+        tabSpecRabbit.setContent(R.id.rabbit);
+        tabHost.addTab(tabSpecRabbit);
+
+        TabHost.TabSpec tabSpecHorse = tabHost.newTabSpec("HORSE").setIndicator("말");
+        tabSpecHorse.setContent(R.id.horse);
+        tabHost.addTab(tabSpecHorse);
+
+        tabHost.setCurrentTab(0);
     }
 }
